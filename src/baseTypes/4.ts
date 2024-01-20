290b8f33498d6b5d24814fbbbe335be737066d44
Oleksii Chubenko <i1723@i.ua>
2024-01-20 22:53:19 +0000

    fixed task 4 and have done tasks 5 and 6

--- src/baseTypes/4.ts
+++ src/baseTypes/4.ts
@@ -3,6 +3,7 @@
   щоб гарантувати, що перший елемент завжди буде рядком, а другий числом?
 */
 
-let person: [string, number] = ['Max', 21];
+type Person = [string, number];
+let person: Person = ['Max', 21];
 
 export {};
